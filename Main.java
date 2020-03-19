f72e26ea27c5b1886758d695670ddf459717e586
Rajeev Kumar <53464007+RajeevKumarThakur@users.noreply.github.com>
2020-03-20 04:18:43 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -75,6 +75,7 @@ public class Main extends Application {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("About Connect Four");
         alert.setHeaderText("How To Play?");
+	    
         alert.setContentText("Connect Four is a two-player connection game in which the players first choose a color" +
                 " and then take turns dropping colored discs from the top into a seven-column," +
                 " six-row vertically suspended grid. The pieces fall straight down," +
@@ -82,6 +83,7 @@ public class Main extends Application {
                 " The objective of the game is to be the first to form a horizontal, vertical," +
                 " or diagonal line of four of one's own discs. Connect Four is a solved game." +
                 " The first player can always win by playing the right moves ");
+	    
         alert.show();
     }
 
